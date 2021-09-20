f78db28ac9d4cd7eca729a087749701b98d56a77
ilovepilav <cagrisakaoglu@gmail.com>
2021-09-20 13:55:25 +0300

    Project Layers Created

---
+++ src/UI/ContactList.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContactList.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
